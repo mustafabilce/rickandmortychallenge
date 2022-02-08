ce0d547d00eeee87d979cdd9399328dede7c3ace
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2022-02-08 17:38:28 +0300

    Locations page created, api integration done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Routes, Route } from "react-router-dom";
-import Locations from "./containers/Locations"
-import Residents from "./containers/Residents"
-import './App.css';
+import Locations from "./containers/Locations";
+import Residents from "./containers/Residents";
+import "./App.css";
+import { Layout } from "antd";
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Locations />} />
-        <Route path="residents" element={<Residents />} />
-      </Routes>
+      <Layout>
+        <Header style={{ backgroundColor: '#f0f2f5', textAlign: "center" }}>
+          Rick and Morty
+        </Header>
+        <Content style={{ padding: '30px', backgroundColor: '#fff' }}>
+          <Routes>
+            <Route path="/" element={<Locations />} />
+            <Route path="residents" element={<Residents />} />
+          </Routes>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Footer
+        </Footer>
+      </Layout>
     </div>
   );
 }
